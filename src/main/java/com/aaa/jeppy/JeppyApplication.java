2b3eeb89b1f4d38340303a9491f8f2e3ae6411b5
Carrot_hyeong <rjgjfl90@naver.com>
2023-06-26 00:52:58 +0900

    Init : jeppy project init - spring boot jdk : 17 spring boot version : 3.1.1 Dependencies : Spring Boot DevTools, Spring Web, Lombok, Spring Data JPA, PostgreSQL Driver, Spring Data Reactive Redis, Spring Security

---
+++ src/main/java/com/aaa/jeppy/JeppyApplication.java
@@ -0,0 +1,13 @@
+package com.aaa.jeppy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeppyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeppyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aaa/jeppy/JeppyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aaa.jeppy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeppyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
