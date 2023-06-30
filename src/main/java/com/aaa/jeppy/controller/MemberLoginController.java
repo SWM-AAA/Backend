0b00decfdabda2bd4c2661ebb45b3b7cc108af22
Carrot_hyeong <rjgjfl90@naver.com>
2023-06-30 16:44:04 +0900

    [TAS-87] Feat : api/auth/check-login (post) api 작성 멤버 가입 여부 확인 api 추 후 가입되어 있는 멤버는 토큰전달하는 코드까지 작성할 예정

---
+++ src/main/java/com/aaa/jeppy/controller/MemberLoginController.java
@@ -0,0 +1,47 @@
+package com.aaa.jeppy.controller;
+
+import com.aaa.jeppy.dto.MemberCheckRegistedDTO;
+import com.aaa.jeppy.service.CheckMemberRegistedService;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class MemberLoginController {
+
+    private final CheckMemberRegistedService checkMemberRegistedService;
+
+    public MemberLoginController(CheckMemberRegistedService checkMemberRegistedService) {
+        this.checkMemberRegistedService = checkMemberRegistedService;
+    }
+
+    @PostMapping("/check-login")
+    public ResponseEntity<Void> memberIsRegistered(@RequestBody MemberCheckRegistedDTO memberCheckRegistedDTO) {
+
+        boolean isRegistered = checkMemberRegistedService.isMemberRegistered(memberCheckRegistedDTO.getMemberEmail());
+
+        if (isRegistered) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/google/register")
+    public ResponseEntity<Void> registerGoogleMember(@RequestBody MemberCheckRegistedDTO memberCheckRegistedDTO) {
+        System.out.println("registerMember: " + memberCheckRegistedDTO.getMemberEmail());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/kakao/register")
+    public ResponseEntity<Void> registerKakaoMember(@RequestBody MemberCheckRegistedDTO memberCheckRegistedDTO) {
+        System.out.println("registerMember: " + memberCheckRegistedDTO.getMemberEmail());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
