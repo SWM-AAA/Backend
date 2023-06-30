aa219df3048ee3ff08c20005a3420401579473d4
Carrot_hyeong <rjgjfl90@naver.com>
2023-06-30 16:42:04 +0900

    Fix : findByEmail 함수 수정으로인한 변경사항

--- src/main/java/com/aaa/jeppy/repository/MemberRepository.java
+++ src/main/java/com/aaa/jeppy/repository/MemberRepository.java
@@ -3,10 +3,8 @@ package com.aaa.jeppy.repository;
 import com.aaa.jeppy.model.Member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Member findByEmailAndProvider(String email, Member.SocialProvider provider);
+    Member findByEmail(String memberEmail);
     
 }
