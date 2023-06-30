eb7e74e9c411d2d670e317bff7c1f0429d5257e5
Carrot_hyeong <rjgjfl90@naver.com>
2023-06-30 16:39:07 +0900

    Feat : spring boot 시작시 연결된 DB에 테스트 유저 정보 삽입하는 코드 작성

---
+++ src/main/java/com/aaa/jeppy/component/MemberDataInitializer.java
@@ -0,0 +1,45 @@
+package com.aaa.jeppy.component;
+
+import com.aaa.jeppy.repository.MemberRepository;
+import com.aaa.jeppy.model.Member;
+import com.aaa.jeppy.model.Member.SocialProvider;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberDataInitializer {
+
+    private final MemberRepository memberRepository;
+
+    public MemberDataInitializer(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    // @PostConstruct
+    // public void init() {
+    //     try {
+    //     Member member1 = new Member();
+
+    //     member1.setEmail("test@naver.com");
+    //     member1.setProvider(SocialProvider.KAKAO);
+    //     member1.setMembername("김덕배");
+        
+    //     memberRepository.save(member1);
+    //     } catch (Exception e) {
+    //         System.out.println(e);
+    //     }
+
+    //     try {
+    //         Member member2 = new Member();
+
+    //         member2.setEmail("test1@naver.com");
+    //         member2.setProvider(SocialProvider.GOOGLE);
+    //         member2.setMembername("손흥민");
+
+    //         memberRepository.save(member2);
+    //     } catch (Exception e) {
+    //         System.out.println(e);
+    //     }
+    // }
+}
