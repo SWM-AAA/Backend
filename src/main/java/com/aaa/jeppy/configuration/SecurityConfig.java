10e6d91a1cb511e399756d610ac0f17b58dd9c3d
Carrot_hyeong <rjgjfl90@naver.com>
2023-06-30 16:40:17 +0900

    Feat : spring boot csrf 토큰 관련 코드 작성중 - spring security

---
+++ src/main/java/com/aaa/jeppy/configuration/SecurityConfig.java
@@ -0,0 +1,13 @@
+// package com.aaa.jeppy.configuration;
+
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+// import HttpSecurity
+
+// @EnableWebSecurity
+// public class SecurityConfig extends WebSecurityConfigurerAdapter{
+//     @Override
+//     protected void configure(HttpSecurity http) throws Exception {
+//         http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+//     }
+// }
