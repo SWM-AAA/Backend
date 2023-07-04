c7b05cf8f06b6169937320e534a57b51e632db05
Carrotww <rjgjfl90@naver.com>
2023-07-04 21:33:56 +0900

    [TAS 101] feat : spring security csrf 비활성화 임시 코드 작성

--- src/main/java/com/aaa/jeppy/configuration/SecurityConfig.java
+++ src/main/java/com/aaa/jeppy/configuration/SecurityConfig.java
@@ -1,13 +1,24 @@
-// package com.aaa.jeppy.configuration;
+package com.aaa.jeppy.configuration;
 
-// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-// import HttpSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import jakarta.servlet.DispatcherType;
 
-// @EnableWebSecurity
-// public class SecurityConfig extends WebSecurityConfigurerAdapter{
-//     @Override
-//     protected void configure(HttpSecurity http) throws Exception {
-//         http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-//     }
-// }
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // csrf 비활성화
+        http.csrf().disable().cors().disable()
+                .authorizeHttpRequests(request -> request
+                	.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                        .anyRequest().authenticated()
+                        // 모든 요청 인증 필요
+                );
+
+        return http.build();
+    }
+}
